251da773d8a735d95ab9277434119e225f95fccd
Paul Kunitsky <paulkunitskybusiness@gmail.com>
2022-03-31 18:06:28 +0300

    clean tests

--- libs/configuration/src/lib/configuration-context.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ConfigurationContext } from './configuration-context';
-
-describe('ConfigurationContext', () => {
-  it('should create an instance', () => {
-    expect(new ConfigurationContext()).toBeTruthy();
-  });
-});
--- libs/logging/src/lib/logging.module.spec.ts
+++
@@ -1,7 +0,0 @@
-import { LoggingModule } from './logging.module';
-
-describe('LoggingModule', () => {
-  it('should work', () => {
-    expect(new LoggingModule()).toBeDefined();
-  });
-});
